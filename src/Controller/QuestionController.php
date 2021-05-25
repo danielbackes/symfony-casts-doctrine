ec2a8deccce3b50e91613313a6a16a49ff26bc27
Daniel Backes <danielbackes@protonmail.com>
2021-05-25 07:55:17 -0400

    feat: create a route to persist a new question entity

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
+use App\Entity\Question;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class QuestionController extends AbstractController
 {
@@ -29,12 +31,51 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager) 
+    {
+        $question = new Question();
+        $question
+            ->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+            Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+            to make my dishes wash themselves. But while I was casting it,
+            I slipped a little and I think `I also hit my pants with the spell`.
+            EOF)
+        ;
+
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug %s',
+            $question->getId(),
+            $question->getSlug(),
+        ));
+    }
+
+    /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
+        }
+
+        $repository = $entityManager->getRepository(Question::class);
+
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers = [
@@ -42,13 +83,9 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-
-        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $parsedQuestionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
