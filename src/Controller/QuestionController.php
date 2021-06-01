1aceca7c3200551f85f7521013c7589b3553b16a
Daniel Backes <danielbackes@protonmail.com>
2021-05-31 20:37:54 -0400

    feat: use paramConverter to recovery the question entity

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -55,6 +55,8 @@ class QuestionController extends AbstractController
             $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
 
+        $question->setVotes(rand(-20, 50));
+
         $entityManager->persist($question);
         $entityManager->flush();
 
@@ -68,19 +70,10 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
-        }
-
-        $repository = $entityManager->getRepository(Question::class);
-
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
-
-        if (!$question) {
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
         }
 
         $answers = [
