3b51deedca33d9b6def8dfc108c463558405a471
Daniel Backes <danielbackes@protonmail.com>
2021-06-01 21:24:10 -0400

    feat: create vote action

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -9,6 +9,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 
 class QuestionController extends AbstractController
@@ -85,6 +86,27 @@ class QuestionController extends AbstractController
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers,
+        ]);
+    }
+
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();          
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug(),
         ]);
     }
 }
